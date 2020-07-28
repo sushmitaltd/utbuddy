0a8ee4c0100d73d0803d846c9308ae4a578211f1
Mahanoor Sayyad <masayyad@microsoft.com>
2020-07-28 11:26:17 +0530

    handling corner cases

--- src/file_path_generator.ts
+++ src/file_path_generator.ts
@@ -2,11 +2,33 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 
+function createAndWriteToFile(filePath:string): void{
+    fs.writeFile(filePath, "content of the file", (error)=>{
+        if (error){
+            console.error('Error occured', error);
+        }
+        else{
+            console.log('Created boiler plate');
+        }
+    });
+}
+
 export default function testFilePathGenerator(filepath:string): void{
-    const indexOfSrc= filepath.indexOf("src");
-    const pathTillSrc= filepath.substring(0,indexOfSrc);
-    const pathAfterSrc = filepath.substring(indexOfSrc+3, filepath.length-2);
-    const new_path = pathTillSrc+'test'+pathAfterSrc+'test.ts';
-    if (!fs.existsSync(new_path)){
-        fs.mkdirSync(new_path, { recursive: true });}
+    const indexOfSrc= filepath.indexOf("\\src\\");
+    const lastDirectoryIndex = filepath.lastIndexOf("\\");
+    const fileName= filepath.substring(lastDirectoryIndex+1, filepath.length-3);
+    let newDirectoryPath:string;
+    
+    if (indexOfSrc>=0){
+        const pathTillSrc= filepath.substring(0,indexOfSrc+1); 
+        const pathAfterSrc = filepath.substring(indexOfSrc+4, lastDirectoryIndex);
+        newDirectoryPath = pathTillSrc+'test'+pathAfterSrc;}       
+    else {
+        // If src is not present
+        newDirectoryPath = filepath.substring(0,lastDirectoryIndex);
+    }
+    if (!fs.existsSync(newDirectoryPath)){
+        fs.mkdirSync(newDirectoryPath, { recursive: true });}
+
+    createAndWriteToFile(newDirectoryPath+"\\"+ fileName+'.test.ts');   
 }
