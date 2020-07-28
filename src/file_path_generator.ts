c6107db90f0d88bed8de555fb22b2fb66fca0623
sushmita-m <sushmita-m@github.com>
2020-07-28 18:50:58 +0530

    added import the file to be tested

--- src/file_path_generator.ts
+++ src/file_path_generator.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 
 
 
-function createAndWriteToFile(newTestFilePath:string, filePath: string): void{
+function createAndWriteToFile(newTestFilePath:string, filePath: string, importPath:string): void{
     let data: string;
     fs.readFile(filePath, "utf8",(err, data) => {
         if (err) {console.error('Error occured', err);}
@@ -12,12 +12,13 @@ function createAndWriteToFile(newTestFilePath:string, filePath: string): void{
             var logger = fs.createWriteStream(newTestFilePath, {
                 flags: 'w' 
               });
-              
             const reg: RegExp = /import.*?;/g;
             let results = data.matchAll(reg);
             for (const match of results){
-                logger.write(match[0]+ '\n');
+                logger.write(match[0]+'\n');
             }
+            // import the file to be tested            
+            logger.write(`import * from \'${importPath.replace('//',`/`)}\' \n`);
         };
       });
 }
@@ -27,10 +28,10 @@ export default function testFilePathGenerator(filepath:string): void{
     const lastDirectoryIndex = filepath.lastIndexOf("\\");
     const fileName= filepath.substring(lastDirectoryIndex+1, filepath.length-3);
     let newDirectoryPath:string;
-    
+    let pathAfterSrc;
     if (indexOfSrc>=0){
         const pathTillSrc= filepath.substring(0,indexOfSrc+1); 
-        const pathAfterSrc = filepath.substring(indexOfSrc+4, lastDirectoryIndex);
+        pathAfterSrc = filepath.substring(indexOfSrc+4, lastDirectoryIndex);
         newDirectoryPath = pathTillSrc+'test'+pathAfterSrc;}       
     else {
         // If src is not present
@@ -39,5 +40,11 @@ export default function testFilePathGenerator(filepath:string): void{
     if (!fs.existsSync(newDirectoryPath)){
         fs.mkdirSync(newDirectoryPath, { recursive: true });}
 
-    createAndWriteToFile(newDirectoryPath+"\\"+ fileName+'.test.ts', filepath);   
+    // import all exports from current file
+    // test + pathaftersrc + filename is the new file location
+    // path after src as  ../ + src + pathaftersrc + filename is the import path
+    const countOfDots = pathAfterSrc.split('\\').length ;
+    const dots = `../`.repeat(countOfDots);
+    const importPath = `${dots}src/${pathAfterSrc}/${fileName}`;
+    createAndWriteToFile(newDirectoryPath+"\\"+ fileName+'.test.ts', filepath, importPath.replace('\\',`/`));   
 }
